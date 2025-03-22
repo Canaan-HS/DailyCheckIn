044d4c0256eacabd992a84e94ff5c1b02ca5f3a9
Canaan-HS <s3459897@gmail.com>
2025-03-23 02:34:46 +0800

    Request to send

---
+++ Modules/Send.py
@@ -0,0 +1,16 @@
+from .__Lib__ import httpx
+
+class CreateSend:
+    def __init__(self):
+        self.Client = httpx.Client(http2=True)
+
+    async def async_http_post(self, Name, Url, Id, Headers, Cookies) -> dict:
+        async with httpx.AsyncClient(http2=True) as client:
+            response = await client.post(
+                Url, json=Id, headers=Headers, cookies=Cookies
+            )
+            return {"Name": Name} | response.json()
+
+    def http_post(self, Url, Headers, Cookies, Data={}):
+        Response = self.Client.post(Url, headers=Headers, cookies=Cookies, json=Data)
+        return Response
