ea3484583eeef0b7850dc0089109330d8a3f4139
Canaan-HS <s3459897@gmail.com>
2025-03-23 02:33:48 +0800

    API

---
+++ Modules/TaskAPI.py
@@ -0,0 +1,23 @@
+# https://pass.levelinfinite.com/
+
+LeveCheckInAPI = {
+    "CheckIn": {
+        "ID": "15",
+        "Url": "https://api-pass.levelinfinite.com/api/rewards/proxy/lipass/Points/DailyCheckIn",
+    },
+    "StageCheckIn": {
+        "ID": "58",
+        "Url": "https://api-pass.levelinfinite.com/api/rewards/proxy/lipass/Points/DailyStageCheckIn",
+    }
+}
+
+LeveStateAPI = {
+    "ViewPoints": "https://api-pass.levelinfinite.com/api/rewards/proxy/lipass/Points/GetUserTotalPoints",
+    "TaskStatus": "https://api-pass.levelinfinite.com/api/rewards/proxy/lipass/Points/GetTaskListWithStatusV2",
+}
+
+# https://discord.com/channels/946719263866101821/1300742310186975232
+
+DiscordAPI = {
+    "SignIn": "https://discord.com/api/v9/interactions"
+}
