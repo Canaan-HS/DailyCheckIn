7b5be743d7f5bab3aedc440576b3b3cdca52b0e5
Canaan-HS <s3459897@gmail.com>
2025-03-24 02:48:40 +0800

    Test

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 from Modules import (
+    os,
     Path,
     Json_Read,
+    Json_Parse,
     CreateTask
 )
 
 current_path = Path(__file__).parent
 
-def LevelinfiniteStart():
-    LevelinfiniteData = Json_Read(current_path / "Data/Levelinfinite.json")
+def LevelinfiniteStart(Local=True):
+    LogPath = current_path / "Log/LeveInfo.log"
+    LevelinfiniteData = Json_Read(current_path / "Data/Levelinfinite.json") if Local else Json_Parse(os.getenv("LevelinfiniteData"))
 
     Task = CreateTask(
+        LogPath,
         LevelinfiniteData["headers"],
         LevelinfiniteData["cookies"]
     )
@@ -17,10 +21,12 @@ def LevelinfiniteStart():
     Task.LeveCheckIn()
     Task.LeveState()
 
-def DiscordStart():
-    DiscordData = Json_Read(current_path / "Data/Discord.json")
+def DiscordStart(Local=True):
+    LogPath = current_path / "Log/DiscordInfo.log"
+    DiscordData = Json_Read(current_path / "Data/Discord.json") if Local else Json_Parse(os.getenv("DiscordData"))
 
     Task = CreateTask(
+        LogPath,
         DiscordData["headers"],
         DiscordData["cookies"],
         DiscordData["data"]
@@ -28,5 +34,5 @@ def DiscordStart():
 
     Task.DiscordSignIn()
 
-LevelinfiniteStart()
-DiscordStart()
+LevelinfiniteStart(False)
+DiscordStart(False)
