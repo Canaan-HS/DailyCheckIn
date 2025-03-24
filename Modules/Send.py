d5263e35d8e5dba18bf72f5765da45c974bb2b1b
Canaan-HS <s3459897@gmail.com>
2025-03-25 00:42:53 +0800

    例外處理

--- Modules/Send.py
+++ Modules/Send.py
@@ -7,7 +7,11 @@ class CreateSend:
     async def async_http_post(self, Name, Url, Headers, Cookies) -> dict:
         async with httpx.AsyncClient(http2=True) as client:
             response = await client.post(Url, headers=Headers, cookies=Cookies)
-            return {"Name": Name} | response.json()
+
+            try:
+                return {"Name": Name} | response.json()
+            except:
+                return {"Name": Name, "response": response.text}
 
     def http_post(self, Url, Headers, Cookies, Data={}):
         Response = self.Client.post(Url, headers=Headers, cookies=Cookies, json=Data)
