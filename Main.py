85a306b909a0e14b278d75d7cf156ff1a2e3bb5c
Canaan-HS <s3459897@gmail.com>
2025-03-25 00:42:31 +0800

    Hoyolab 測試

--- Main.py
+++ Main.py
@@ -56,4 +56,4 @@ if __name__ == "__main__":
     # Local 設置為 False 時, 通常無法在本地運行, 除非有額外設置 (主要用於 GitHub Actions)
     LevelinfiniteStart(False)
     DiscordStart(False)
-    # Hoyolab()
+    Hoyolab(False)
