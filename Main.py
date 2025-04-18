6c7ceb1d4d9ecfb41987a50c0da69e92da72e425
Canaan-HS <s3459897@gmail.com>
2025-04-19 06:19:45 +0800

    調整調用方式

--- Main.py
+++ Main.py
@@ -1,59 +1,62 @@
-from Modules import (
-    os,
-    Path,
-    Json_Read,
-    Json_Parse,
-    CreateTask
-)
+from Modules import os, Path, Json_Read, Json_Parse, CreateTask
 
 current_path = Path(__file__).parent
 
-def LevelinfiniteStart(Local=True):
+"""
+? 程式使用描述
+
+Todo - 1. 根據要簽到的功能, 呼叫對應函數
+Todo - 2. 如果是本地使用, 不需要傳遞任何參數
+Todo - 3. 如果是環境變數使用, 可設置 EnvSource 參數 與 LogSave 參數
+
+! (參數並無做檢查, 如果 EnvSource 不存在, 就直接壞掉)
+"""
+
+
+def LevelinfiniteStart(EnvSource: str = None, LogSave: str = "Log/LeveInfo.log"):
     LogPath, LevelinfiniteData = (
-        (None, Json_Read(current_path / "Data/Levelinfinite.json"))
-        if Local else (current_path / "Log/LeveInfo.log", Json_Parse(os.getenv("LevelinfiniteData")))
+        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
+        if isinstance(EnvSource, str)
+        else (None, Json_Read(current_path / "Data/Levelinfinite.json"))
     )
 
-    Task = CreateTask(
-        LogPath,
-        LevelinfiniteData['headers'],
-        LevelinfiniteData['cookies']
-    )
+    Task = CreateTask(LogPath, LevelinfiniteData["headers"], LevelinfiniteData["cookies"])
 
     Task.LeveCheckIn()
     Task.LeveState()
 
-def DiscordStart(Local=True):
+
+def DiscordStart(EnvSource: str = None, LogSave: str = "Log/DiscordInfo.log"):
     LogPath, DiscordData = (
-        (None, Json_Read(current_path / "Data/Discord.json"))
-        if Local else (current_path / "Log/DiscordInfo.log", Json_Parse(os.getenv("DiscordData")))
+        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
+        if isinstance(EnvSource, str)
+        else (None, Json_Read(current_path / "Data/Discord.json"))
     )
 
-    Task = CreateTask(
-        LogPath,
-        DiscordData['headers'],
-        DiscordData['cookies'],
-        DiscordData['data']
-    )
+    Task = CreateTask(LogPath, DiscordData["headers"], DiscordData["cookies"], DiscordData["data"])
 
     Task.DiscordSignIn()
 
-def Hoyolab(Local=True):
+
+def Hoyolab(EnvSource: str = None, LogSave: str = "Log/HoyolabInfo.log"):
     LogPath, HoyolabData = (
-        (None, Json_Read(current_path / "Data/Hoyolab.json"))
-        if Local else (current_path / "Log/HoyolabInfo.log", Json_Parse(os.getenv("HoyolabData")))
+        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
+        if isinstance(EnvSource, str)
+        else (None, Json_Read(current_path / "Data/Hoyolab.json"))
     )
 
-    Task = CreateTask(
-        LogPath,
-        HoyolabData['headers'],
-        HoyolabData['cookies']
-    )
+    Task = CreateTask(LogPath, HoyolabData["headers"], HoyolabData["cookies"])
 
     Task.HoyolabCheckIn()
 
+
 if __name__ == "__main__":
-    # Local 設置為 False 時, 通常無法在本地運行, 除非有額外設置 (主要用於 GitHub Actions)
-    # LevelinfiniteStart()
-    DiscordStart(False)
+    # 本地端運行
     # Hoyolab()
+    # DiscordStart()
+    # LevelinfiniteStart()
+
+    # GitHub Actions 運行
+    # Hoyolab("HoyolabData")
+    DiscordStart("DiscordData")
+    # LevelinfiniteStart("LevelinfiniteData")
