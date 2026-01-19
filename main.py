351019541545a6397cf88c2c65f9746f97d5399e
Canaan-HS <s3459897@gmail.com>
2026-01-19 12:40:11 +0800

    大小寫調整

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Modules import os, Path, Json_Read, Json_Parse, CreateTask
+from Modules import os, Path, json_read, json_parse, CreateTask
 
 current_path = Path(__file__).parent
 
@@ -7,57 +7,57 @@ current_path = Path(__file__).parent
 
 Todo - 1. 根據要簽到的功能, 呼叫對應函數
 Todo - 2. 如果是本地使用, 不需要傳遞任何參數
-Todo - 3. 如果是環境變數使用, 可設置 EnvSource 參數 與 LogSave 參數
+Todo - 3. 如果是環境變數使用, 可設置 env_source 參數 與 log_save_path 參數
 
-! 參數並無做檢查, 如果 EnvSource 不存在, 或數據格式不符合模板, 就直接壞掉
+! 參數並無做檢查, 如果 env_source 不存在, 或數據格式不符合模板, 就直接壞掉
 """
 
 
-def LevelinfiniteStart(EnvSource: str = None, LogSave: str = "Log/LeveInfo.log"):
-    LogPath, LevelinfiniteData = (
-        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
-        if isinstance(EnvSource, str)
-        else (None, Json_Read(current_path / "Data/Levelinfinite.json"))
+def levelinfinite_start(env_source: str = None, log_save_path: str = "Log/LeveInfo.log"):
+    log_path, levelinfinite_data = (
+        (current_path / log_save_path, json_parse(os.getenv(env_source)))
+        if isinstance(env_source, str)
+        else (None, json_read(current_path / "Data/Levelinfinite.json"))
     )
 
-    Task = CreateTask(LogPath, LevelinfiniteData["headers"], LevelinfiniteData["cookies"])
+    task = CreateTask(log_path, levelinfinite_data["headers"], levelinfinite_data["cookies"])
 
-    Task.LeveCheckIn()
-    Task.LeveState()
+    task.leve_checkIn()
+    task.leve_state()
 
 
-def DiscordStart(EnvSource: str = None, LogSave: str = "Log/DiscordInfo.log"):
-    LogPath, DiscordData = (
-        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
-        if isinstance(EnvSource, str)
-        else (None, Json_Read(current_path / "Data/Discord.json"))
+def discord_start(env_source: str = None, log_save_path: str = "Log/DiscordInfo.log"):
+    log_path, discord_data = (
+        (current_path / log_save_path, json_parse(os.getenv(env_source)))
+        if isinstance(env_source, str)
+        else (None, json_read(current_path / "Data/Discord.json"))
     )
 
-    Task = CreateTask(LogPath, DiscordData["headers"], DiscordData["cookies"])
+    task = CreateTask(log_path, discord_data["headers"], discord_data["cookies"])
 
-    Task.NikkeDiscordSignIn(DiscordData["signin-data"])
-    # Task.NikkeInputCdKey(DiscordData["cdkey-data"])
+    task.nikke_discord_signIn(discord_data["signin-data"])
+    # task.nikke_input_cdKey(discord_data["cdkey-data"])
 
 
-def Hoyolab(EnvSource: str = None, LogSave: str = "Log/HoyolabInfo.log"):
-    LogPath, HoyolabData = (
-        (current_path / LogSave, Json_Parse(os.getenv(EnvSource)))
-        if isinstance(EnvSource, str)
-        else (None, Json_Read(current_path / "Data/Hoyolab.json"))
+def hoyolab(env_source: str = None, log_save_path: str = "Log/HoyolabInfo.log"):
+    log_path, hoyolab_data = (
+        (current_path / log_save_path, json_parse(os.getenv(env_source)))
+        if isinstance(env_source, str)
+        else (None, json_read(current_path / "Data/Hoyolab.json"))
     )
 
-    Task = CreateTask(LogPath, HoyolabData["headers"], HoyolabData["cookies"])
+    task = CreateTask(log_path, hoyolab_data["headers"], hoyolab_data["cookies"])
 
-    Task.HoyolabCheckIn()
+    task.hoyolab_checkIn()
 
 
 if __name__ == "__main__":
     # ? 本地端運行
-    # Hoyolab()
-    # DiscordStart()
-    # LevelinfiniteStart()
+    # hoyolab()
+    # discord_start()
+    # levelinfinite_start()
 
     # ? GitHub Actions 運行
-    # Hoyolab("HoyolabData")
-    DiscordStart("DiscordData")
-    # LevelinfiniteStart("LevelinfiniteData")
+    # hoyolab("HoyolabData")
+    discord_start("DiscordData")
+    # levelinfinite_start("LevelinfiniteData")
