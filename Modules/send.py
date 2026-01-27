c71ee08733e5d71cdeeb12594f958d7b907bff43
Canaan-HS <s3459897@gmail.com>
2026-01-27 16:50:09 +0800

    Change the use of httpx to using curl_cffi

--- Modules/send.py
+++ Modules/send.py
@@ -1,23 +1,23 @@
-from .libs import httpx
+from .libs import curl, CurlAsyncSession
 
 
 class CreateSend:
     def __init__(self):
-        self.Client = httpx.Client(http2=True)
+        self.session = curl.Session(impersonate="chrome120")
 
-    async def async_http_post(self, name, url, headers, cookies) -> dict:
-        async with httpx.AsyncClient(http2=True) as client:
-            response = await client.post(url, headers=headers, cookies=cookies)
+    async def async_curl_post(self, name, url, headers, cookies) -> dict:
+        async with CurlAsyncSession(impersonate="chrome120") as session:
+            response = await session.post(url, headers=headers, cookies=cookies)
 
             try:
                 return {"Name": name} | response.json()
             except:
                 return {"Name": name, "response": response.text}
 
-    def http_get(self, url, headers, cookies):
-        response = self.Client.get(url, headers=headers, cookies=cookies)
+    def curl_get(self, url, headers, cookies):
+        response = self.session.get(url, headers=headers, cookies=cookies)
         return response
 
-    def http_post(self, url, headers, cookies, Data={}):
-        response = self.Client.post(url, headers=headers, cookies=cookies, json=Data)
+    def curl_post(self, url, headers, cookies, Data={}):
+        response = self.session.post(url, headers=headers, cookies=cookies, json=Data)
         return response
